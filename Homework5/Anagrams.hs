a16518fddbb3447e049edc6cd542f565b8ccd176
Alexander Conley <alexanderconley@gmail.com>
2014-03-02 17:45:05 -0700

    Add subtract method

--- Homework5/Anagrams.hs
+++ Homework5/Anagrams.hs
@@ -8,6 +8,7 @@ module Anagrams where
 
 import Data.List (groupBy, sort)
 import Data.Char (toLower)
+import Control.Applicative ((<$>))
 
 {-
   An occurence list is a list of char, int pairs, sorted by
@@ -16,7 +17,7 @@ import Data.Char (toLower)
   be positive, and the character is kept in lower case.
   Each character should only appear once.
 -}
-data Occurrence = Occurrence Char Int deriving (Eq)
+data Occurrence = Occurrence { char :: Char, getCount :: Int } deriving (Eq)
 type Occurrences = [Occurrence]
 
 -- Occurrence lists are sorted by the character
@@ -64,3 +65,38 @@ combinations (x:xs) =
         prev_sub = combinations xs
         prev_plus_new = [ e : p | e <- this_sub, p <- prev_sub ]
     in prev_sub ++ prev_plus_new
+
+{-
+  This is the subtraction accumulator for sub.  The output is
+  accumulated in reverse order for efficiency.
+-}
+subAcc :: Occurrences -> Occurrences -> Maybe Occurrences -> Maybe Occurrences
+subAcc _ _ Nothing = Nothing
+subAcc [] [] acc = acc  -- All done, lists consumed
+subAcc [] subtrahend _ = Nothing -- subtrahend was not subset of minuend
+subAcc minuend [] (Just acc) =  -- Done processing subtrahend
+    let revminu = reverse minuend
+    in Just (revminu ++ acc)
+subAcc (m:ms) subt@(s:ss) (Just acc) =   --Most interesting case
+  case compare m s of GT -> Nothing   -- subtrahend not subset of minuend
+                      LT -> subAcc ms subt $ Just $ m : acc
+                      EQ -> let c = char m
+                                n1 = getCount m
+                                n2 = getCount s
+                            in case compare n1 n2 of
+                              GT -> let newOcc = Occurrence c (n1 - n2)
+                                    in subAcc ms ss $ Just $ newOcc : acc
+                              LT -> Nothing  -- again, not a subset
+                              EQ -> subAcc ms ss (Just acc)
+
+{-
+  Subtraction of two occurrence lists.  Uses Maybe to handle
+  failure, which occurs when the second object is not a strict
+  subset of the first.
+
+  As an example:
+     [('a', 2), ('b', 1)] - [('a', 1), ('b', 1)] = Just [('a', 1)]
+-}
+sub :: Occurrences -> Occurrences -> Maybe Occurrences
+minuend `sub` subtrahend = 
+    reverse <$> subAcc minuend subtrahend (Just []) 
--- Homework5/Homework5.hs
+++ Homework5/Homework5.hs
@@ -48,10 +48,38 @@ test_comb = TestCase $ do
   assertEqual "combinations of [('a', 2), ('b', 1)] should be as expected"
      (combinations test_occlist) (expl)
 
+test_sub = TestCase $ do
+  let x = [Occurrence 'a' 1, Occurrence 'd' 2, Occurrence 'l' 1,
+           Occurrence 'r' 1]
+      y1 = [Occurrence 'r' 1]
+      y2 = [Occurrence 'a' 1, Occurrence 'l' 1]
+      y3 = [Occurrence 'd' 1, Occurrence 'l' 1, Occurrence 'r' 1]
+      y4 = [Occurrence 'a' 1, Occurrence 'd' 2]
+      y5 = [Occurrence 'a' 1, Occurrence 'c' 4, Occurrence 'l' 1]
+  assertEqual "Subtracting empty list should do nothing"
+    (x `sub` []) (Just x)
+  assertEqual "One element subtraction should remove just that"
+    (x `sub` y1) (Just [Occurrence 'a' 1, Occurrence 'd' 2, Occurrence 'l' 1])
+  assertEqual "Two element subtraction should remove those two"
+    (x `sub` y2) (Just [Occurrence 'd' 2, Occurrence 'r' 1])
+  assertEqual "Three element partial subtraction should remove and reduce"
+    (x `sub` y3) (Just [Occurrence 'a' 1, Occurrence 'd' 1])
+  assertEqual "Should subtract first to elements"
+    (x `sub` y4) (Just [Occurrence 'l' 1, Occurrence 'r' 1])
+  assertEqual "Subtraction of element not in list should result in Nothing"
+    (x `sub` [Occurrence 'c' 4]) (Nothing)
+  assertEqual "Subtraction buried element not in list should be Nothing"
+    (x `sub` y5) (Nothing)
+  assertEqual "Subtraction of element with larger count should be Nothing"
+    (x `sub` [Occurrence 'd' 3]) (Nothing)
+  assertEqual "Self subtraction should give empty list"
+    (x `sub` x) (Just [])
+
 tests = TestList [TestLabel "test Ordering" test_ord,
                   TestLabel "test makeWordOccurrences" test_makewo,
                   TestLabel "test makeSentenceOccurrences" test_makeso,
                   TestLabel "test generateSubOccurrence" test_gensub,
-                  TestLabel "test combinations" test_comb]
+                  TestLabel "test combinations" test_comb,
+                  TestLabel "test subtraction" test_sub]
 
 main = runTestTT tests
