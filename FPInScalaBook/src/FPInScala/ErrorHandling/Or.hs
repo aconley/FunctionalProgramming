2c1a9ce054522c86215a19d908c9fd85f3accdc7
Alexander Conley <alexanderconley@gmail.com>
2015-05-26 21:30:47 -0600

    Start Either (called Or)

---
+++ FPInScalaBook/src/FPInScala/ErrorHandling/Or.hs
@@ -0,0 +1,41 @@
+module FPInScala.ErrorHandling.Or (
+    Or(..),
+    orElse,
+    map2
+    ) where
+
+import Control.Applicative
+
+-- Use the name Or instead of Either, which is a built-in in Haskell
+--  and First for Left, Second for Right
+
+-- Either in Prelude
+data Or a b = First a | Second b deriving (Show, Eq)
+
+instance Functor (Or a) where
+    fmap _ (First x) = (First x)
+    fmap f (Second x) = Second (f x)
+
+instance Applicative (Or a) where
+    pure = Second
+
+    Second f <*> m = fmap f m
+    First x <*> _ = First x
+
+instance Monad (Or a) where
+    Second x >>= f = f x
+    First x >>= _ = First x
+
+    return = Second
+
+-- Return whichever argument is not First
+orElse :: Or a b -> Or a b -> Or a b
+orElse (First _) d = d
+orElse (Second x) _ = Second x
+
+map2 :: (a -> b -> c) -> Or e a -> Or e b -> Or e c
+map2 _ (First x) _ = First x
+map2 _ _ (First x) = First x
+map2 f (Second x) (Second y) = Second $ f x y
+
+
