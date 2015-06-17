acd16de703a8bd79526cb2a1bd0f6b0d06ac1275
Alexander Conley <alexanderconley@gmail.com>
2015-06-16 19:28:01 -0600

    Fix candy machine test

--- FPInScalaBook/src/tests/CandyMachineTests.hs
+++ FPInScalaBook/src/tests/CandyMachineTests.hs
@@ -7,9 +7,9 @@ import FPInScala.State.CandyMachine
 
 test_simulatemachine = TestCase $ do
     let inp = [Coin, Turn, Coin, Turn, Coin, Turn]
-        init_state = Machine True 10 5
+        init_state = Machine True 5 10 -- 5 candies, 10 coins
         machine = simulateMachine inp
-    assertEqual "Should have 14, 1 candies, coins"
-        (14, 1) (fst $ runState machine init_state)
+    assertEqual "Should have 2 candies, 13 coins"
+        (2, 13) (fst $ runState machine init_state)
 
 candy_machine_tests = [TestLabel "test simulateMachine" test_simulatemachine]
